dcf0e8c5beec95e7363cd79f1ca66eda28ec4da9
Muhammad Asif Iqbal <asifmiqbal.pk@gmail.com>
2024-02-21 14:49:16 +0500

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     contentArea.addEventListener('click', function(e) {
-    // Check if sidebar is active and the clicked target is not the sidebar or toggle button
+        // Check if sidebar is active and the clicked target is not the sidebar or toggle button
         if (sidebar.classList.contains('active') && !sidebar.contains(e.target) && e.target !== toggleButton) {
             sidebar.classList.remove('active');
         }
@@ -42,51 +42,47 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     });
+
+    // Initialize Owl Carousel
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        items:1,
+        loop:true,
+        margin:10,
+        autoplay:true,
+        autoHeight:false,
+        autoplayTimeout:5000,
+        autoplayHoverPause:true
+    });
+    $('.play').on('click',function(){
+        owl.trigger('play.owl.autoplay',[1000])
+    });
+    $('.stop').on('click',function(){
+        owl.trigger('stop.owl.autoplay')
+    });
 });
 
-
-//Owl Carousel Initialization and Controls//
-var owl = $('.owl-carousel');
-owl.owlCarousel({
-    items:1,
-    loop:true,
-    margin:10,
-    autoplay:true,
-    autoHeight:false,
-    autoplayTimeout:5000,
-    autoplayHoverPause:true
-});
-$('.play').on('click',function(){
-    owl.trigger('play.owl.autoplay',[1000])
-})
-$('.stop').on('click',function(){
-    owl.trigger('stop.owl.autoplay')
-})
-
-
-// Modal Functionality for Portfolio Images //
-
-    $(document).ready(function(){
-        // Select all images within the portfolio-wrapper class and add click event
-        $('.portfolio-wrapper img').click(function(){
-            var src = $(this).attr('src'); // Get the source of the clicked image
-            $('#modalImage').attr('src', src); // Set the source of the modal image to the clicked image's source
-            $('#imageModal').css('display', 'block'); // Display the modal
-        });
-
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
-
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() { 
-            $('#imageModal').css('display', 'none');
-        }
-
-        // Also close the modal if the user clicks anywhere outside of the modal image
-        window.onclick = function(event) {
-            if (event.target == document.getElementById('imageModal')) {
-                $('#imageModal').css('display', 'none');
-            }
-        }
+// Modal Functionality for Portfolio Images
+$(document).ready(function(){
+    // Select all images within the portfolio-wrapper class and add click event
+    $('.portfolio-wrapper img').click(function(){
+        var src = $(this).attr('src'); // Get the source of the clicked image
+        $('#modalImage').attr('src', src); // Set the source of the modal image to the clicked image's source
+        $('#imageModal').css('display', 'block'); // Display the modal
     });
 
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() { 
+        $('#imageModal').css('display', 'none');
+    };
+
+    // Also close the modal if the user clicks anywhere outside of the modal image
+    window.onclick = function(event) {
+        if (event.target == document.getElementById('imageModal')) {
+            $('#imageModal').css('display', 'none');
+        }
+    };
+});
