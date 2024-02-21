593078a20e3e8299f263e867838cc0ecb5fbef7d
Muhammad Asif Iqbal <asifmiqbal.pk@gmail.com>
2024-02-21 14:31:56 +0500

    changes in script and div

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
+    
     // Sidebar toggle functionality
-    document.getElementById('toggle-button').addEventListener('click', function() {
-        document.getElementById('sidebar').classList.toggle('active');
+    const toggleButton = document.getElementById('toggle-button');
+    const sidebar = document.getElementById('sidebar');
+    const contentArea = document.querySelector('body'); // Selecting the body as the area to listen for clicks
+
+    toggleButton.addEventListener('click', function() {
+        sidebar.classList.toggle('active');
     });
 
+    contentArea.addEventListener('click', function(e) {
+    // Check if sidebar is active and the clicked target is not the sidebar or toggle button
+        if (sidebar.classList.contains('active') && !sidebar.contains(e.target) && e.target !== toggleButton) {
+            sidebar.classList.remove('active');
+        }
+    });
+   
     // Enhanced navigation handling for portfolio-specific sections
     const portfolioLinks = document.querySelectorAll('.portfolio-nav ul li a');
 
@@ -28,13 +40,12 @@ document.addEventListener('DOMContentLoaded', function() {
                     console.error('Target section not found: ' + targetId);
                 }
             }
-            // External links work as normal, no need for preventDefault() or additional logic
         });
     });
 });
 
 
-
+//Owl Carousel Initialization and Controls//
 var owl = $('.owl-carousel');
 owl.owlCarousel({
     items:1,
@@ -53,5 +64,29 @@ $('.stop').on('click',function(){
 })
 
 
+// Modal Functionality for Portfolio Images //
 
+    $(document).ready(function(){
+        // Select all images within the portfolio-wrapper class and add click event
+        $('.portfolio-wrapper img').click(function(){
+            var src = $(this).attr('src'); // Get the source of the clicked image
+            $('#modalImage').attr('src', src); // Set the source of the modal image to the clicked image's source
+            $('#imageModal').css('display', 'block'); // Display the modal
+        });
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() { 
+            $('#imageModal').css('display', 'none');
+        }
+
+        // Also close the modal if the user clicks anywhere outside of the modal image
+        window.onclick = function(event) {
+            if (event.target == document.getElementById('imageModal')) {
+                $('#imageModal').css('display', 'none');
+            }
+        }
+    });
 
